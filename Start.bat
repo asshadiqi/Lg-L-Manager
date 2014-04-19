4dca2b8b6f464b2379ee245c2da06db633f551ab
ionolinuxoparty <io.nolinuxnoparty@gmail.com>
2014-04-19 14:38:38 +0200

    Update 1

--- Start.bat
+++ Start.bat
@@ -1,31 +1,12 @@
 @echo off
 
-%DEVICE%=none
-%ROOT%=C:\Lg-Manager\res\root.zip
-%L3%=C:\Lg-Manager\L3
-%L7%=C:\Lg-Manager\L7
-%L5%=C:\Lg-Manager\L5
-
-######################################################################
-
-#Device: [1] e400 > L3, [3] p700 > L7, [2] e61x > L5
 cls
 goto :device
 
 ######################################################################
 
-:device
-echo.
-echo.
-echo Select your device
-echo.
-echo Lg L3 e400 = L3
-echo Lg L5 e61x = L5
-echo Lg L7 p700 = L3
 
-set /p DEVICE= [L3] [L5] [L7]: 
-goto :startup
-
+:startup
 
 cls
 echo ################################
@@ -33,21 +14,22 @@ echo # LG L Manager                 #
 echo # Selected device: %DEVICE%    #
 echo ################################
 echo.
-echo 1- Install         4- Reboot
+echo 1- Install         4- Full Unlock
 echo 2- Backup          5- Shell
 echo 3- Push and Pull   6- Settings
 echo .
-echo 0- Exit
+echo 0- Exit            00-About..
 echo.
 echo.
 set /p S= ? :
 if %S%==1 goto :install
 if %S%==2 goto :backupc
 if %S%==3 goto :sync
-if %S%==4 goto :rebootd
+if %S%==4 goto :unlock1
 if %S%==5 goto :terminal
 if %S%==6 goto :setup
 if %S%==0 exit
+if %S%==0 goto :about
 echo Invalid Input? Try again!...
 pause
 goto :startup
@@ -65,19 +47,16 @@ echo ################################
 echo.
 echo Installer
 echo.
-echo 1- Apk       4- Repartition
-echo 2- Rom       5- Gapps/Mod
-echo 3- Recovery  6- Root
+echo 1- Apk       3- Mod/Gapps
+echo 2- Rom       4- Root
 echo.
 echo 0- Go back
 echo.
 set /p S= ? :
 if %S%==1 goto :apk
 if %S%==2 goto :rom
-if %S%==3 goto :rec1
-if %S%==4 goto :rep1
-if %S%==5 goto :mod
-if %S%==6 goto :root
+if %S%==3 goto :mod
+if %S%==4 goto :root
 if %S%==0 goto :startup
 echo Invalid Input? Try again!...
 pause
@@ -117,86 +96,6 @@ pause
 goto :startup
 
 
-:rec1
-if %DEVICE%==1 goto :rec2
-if %DEVICE%==2 goto :rec3
-
-:rec2
-cls
-echo ################################
-echo # LG L Manager                 #
-echo # Selected device: %DEVICE%    #
-echo ################################
-echo.
-echo Recovery installer for Xiaomi mi2(s)
-echo.
-@adb reboot bootloader
-@fastboot devices
-@fastboot flash recovery C:\XiaomiTool\Aries\Recovery\recovery.img
-echo Done!
-@fastboot reboot
-goto :startup
-
-
-:rec3
-cls
-echo ################################
-echo # LG L Manager                 #
-echo # Selected device: %DEVICE%    #
-echo ################################
-echo.
-echo Recovery installer for Xiaomi mi3
-echo.
-@adb reboot bootloader
-@fastboot devices
-@fastboot flash recovery C:\XiaomiTool\Cancro\Recovery\recovery.img
-echo Done!
-@fastboot reboot
-goto :startup
-
-
-:rep1
-if %DEVICE%==1 goto :rep2
-if %DEVICE%==2 goto :rep3
-
-:rep2
-cls
-echo ################################
-echo # LG L Manager                 #
-echo # Selected device: %DEVICE%    #
-echo ################################
-echo.
-echo Repartition for Xiaomi mi2(s)
-echo.
-@adb reboot recovery
-@adb wait-for-device
-pause
-@adb pull C:\XiaomiTool\Aries\Repartition.sh /tmp/
-@adb shell chmod 0777 /tmp/repartition.sh
-@adb shell sh /tmp/repartition.sh
-echo Now you MUST install a rom. Let's sideload it
-pause
-goto :rom
-
-:rep2
-cls
-echo ################################
-echo # LG L Manager                 #
-echo # Selected device: %DEVICE%    #
-echo ################################
-echo.
-echo Repartition for Xiaomi mi3
-echo.
-@adb reboot recovery
-@adb wait-for-device
-pause
-@adb pull C:\XiaomiTool\Cancro\Repartition.sh /tmp/
-@adb shell chmod 0777 /tmp/repartition.sh
-@adb shell sh /tmp/repartition.sh
-echo Now you MUST install a rom. Let's sideload it
-pause
-goto :rom
-
 :mod
 cls
 echo ################################
@@ -211,12 +110,10 @@ SET /P MOD= Drag your zip file here, then press Enter:
 @adb wait-for-device
 @adb shell rm -rf /cache/recovery
 @adb shell mkdir /cache/recovery
-adb shell "echo -e '--sideload' > /cache/recovery/command"
-adb reboot recovery
-adb wait-for-device
-echo Wait until you'll see an updating android picture on your phone, then
-pause
-adb sideload %MOD%
+@adb shell "echo -e '--sideload' > /cache/recovery/command"
+@adb reboot recovery
+@adb wait-for-device
+@adb sideload %MOD%
 echo Phone will apply the update, do not reboot it untit it ends
 pause
 goto :startup
@@ -240,7 +137,7 @@ adb reboot recovery
 adb wait-for-device
 echo Wait until you'll see an updating android picture on your phone, then
 pause
-adb sideload C:\XiaomiTool\res\root.zip
+adb sideload C:\Lg-Manager\res\root.zip
 echo Phone will apply the update, do not reboot it untit it ends
 pause
 goto :startup
@@ -248,11 +145,19 @@ goto :startup
 ##########Settings
 
 :device
-echo.
+cls
+echo ################################
+echo # LG L Manager                 #
+echo # Selected device: %DEVICE%    #
+echo ################################
 echo.
 echo Select your device
 echo.
-set /p DEVICE= 1- Xiaomi Mi2(s)      2- Xiaomi Mi3    :
+echo Lg L3 e400 = 1
+echo Lg L5 e610 = 2
+echo Lg L7 p700 = 3
+echo Lg L3 e400 = 4
+set /p DEVICE= [1] [2] [3] [4]: 
 goto :startup
 
 
@@ -262,7 +167,7 @@ goto :startup
 :backupc
 cls
 echo ################################
-echo # Xiaomi Toolkit               #
+echo # LG L Manager                 #
 echo # Selected device: %DEVICE%    #
 echo ################################
 echo.
@@ -288,7 +193,7 @@ echo.
 echo Backup
 echo.
 set /p BAK=Write here your backup name (NO spaces): 
-@adb backup -nosystem -noshared -apk -f C:\XiaomiTool\Backups\%BAK%.ab
+@adb backup -nosystem -noshared -apk -f C:\Lg-Manager\Backups\%BAK%.ab
 echo Select your password (on phone) if you want, and wait untilt it works.
 pause
 goto :startup
@@ -303,7 +208,7 @@ echo.
 echo Restore
 echo.
 set /p BAK=Write here your backup name (NO spaces): 
-@adb restore C:\XiaomiTool\Backups\%BAK%.ab
+@adb restore C:\Lg-Manager\Backups\%BAK%.ab
 echo Type your password (on phone), and wait untilt it works.
 pause
 goto :startup
@@ -352,38 +257,12 @@ echo ################################
 echo.
 echo Import Photos and Videos
 echo.
-echo File will be placed inside C:\XiaomiTool\Userfiles\Camera
+echo Files will be placed inside C:\Lg-Manager\Userfiles\Camera
 pause
-@adb pull /sdcard/DCIM/Camera C:\XiaomiTool\Userfiles\
+@adb pull /sdcard/DCIM/Camera C:\Lg-Manager\Userfiles\
 pause
 goto :starup
 
-
-#############Reboot
-
-:rebootd
-cls
-echo ################################
-echo # LG L Manager                 #
-echo # Selected device: %DEVICE%    #
-echo ################################
-echo.
-echo Reboot..
-echo.
-echo 1- System        3- Bootloader
-echo 2- Recovery      4- Download
-echo.
-echo 0- Go back
-echo.
-set /p S= ? :
-if %S%==1 @adb reboot & goto :startup
-if %S%==2 @adb reboot recovery & goto:startup
-if %S%==3 @adb reboot bootloader & goto:startup
-if %S%==4 @adb reboot dload & goto:startup
-if %S%==0 goto :startup
-echo Invalid Input? Try again!...
-pause
-goto :rebootd
 
 
 #############Shell
@@ -402,3 +281,170 @@ adb shell
 pause
 goto :startup
 
+
+#############Unlock
+:unlock1
+if %DEVICE%==1 goto :unlock3
+if %DEVICE%==2 goto :unlock5
+if %DEVICE%==3 goto :unlock7
+if %DEVICE%==4 goto :unlock32
+
+:unlock3
+cls
+echo ################################
+echo # LG L Manager                 #
+echo # Selected device: %DEVICE%    #
+echo ################################
+echo.
+echo Full unlock - Lg l3 e400
+echo.
+echo Unlocker by linuxxxx
+@adb reboot bootloader
+@fastboot devices
+@fastboot flash recovery C:\Lg-Manager\L3\recovery\recovery.img
+@fastboot boot C:\Lg-Manager\L3\recovery\recovery.img
+@adb wait-for-device
+@adb shell rm -rf /cache/recovery
+@adb shell mkdir /cache/recovery
+@adb shell "echo -e '--sideload' > /cache/recovery/command"
+@adb reboot bootloader
+@fastboot boot C:\Lg-Manager\L3\recovery\recovery.img
+@adb wait-for-device
+@adb sideload C:\Lg-Manager\res\root.zip
+echo Now phone will complete unlock.
+echo When it will become blank / says done
+pause
+@adb reboot
+echo Done! Succesfully unlocked!
+goto :startup
+
+:unlock5
+cls
+echo ################################
+echo # LG L Manager                 #
+echo # Selected device: %DEVICE%    #
+echo ################################
+echo.
+echo Full unlock - Lg l5 e610
+echo.
+echo Unlocker by linuxxxx
+@adb reboot bootloader
+@fastboot devices
+@fastboot boot C:\Lg-Manager\L5\recovery\recovery.img
+@adb wait-for-device
+@adb push C:\Lg-Manager\L5/bootloader/emmc_appsboot.bin /sdcard/
+@adb shell dd if=/sdcard/emmc_appsboot.bin of=/dev/block/mmcblk0p5
+@adb reboot bootloader
+@fastboot devices
+@fastboot flash recovery C:\Lg-Manager\L5\recovery\recovery.img
+@fastboot boot C:\Lg-Manager\L5\recovery\recovery.img
+@adb shell rm -rf /cache/recovery
+@adb shell mkdir /cache/recovery
+@adb shell "echo -e '--sideload' > /cache/recovery/command"
+@adb reboot bootloader
+@fastboot boot C:\Lg-Manager\L5\recovery\recovery.img
+@adb wait-for-device
+@adb sideload %ROOT%
+echo Now phone will complete unlock.
+echo When it will become blank / says done
+pause
+@adb reboot
+echo Done! Succesfully unlocked!
+goto :startup
+
+
+:unlock7
+cls
+echo ################################
+echo # LG L Manager                 #
+echo # Selected device: %DEVICE%    #
+echo ################################
+echo.
+echo Full unlock - Lg l7 p700
+echo.
+echo Unlocker by linuxxxx
+@adb reboot bootloader
+@fastboot devices
+@fastboot boot C:\Lg-Manager\L7\recovery\recovery.img
+@adb wait-for-device
+@adb push C:\Lg-Manager\L7/bootloader/emmc_appsboot.bin /sdcard/
+@adb shell dd if=/sdcard/emmc_appsboot.bin of=/dev/block/mmcblk0p5 bs=4096
+@adb reboot bootloader
+@fastboot devices
+@fastboot flash recovery C:\Lg-Manager\L5\recovery\recovery.img
+@fastboot boot C:\Lg-Manager\L7\recovery\recovery.img
+@adb shell rm -rf /cache/recovery
+@adb shell mkdir /cache/recovery
+@adb shell "echo -e '--sideload' > /cache/recovery/command"
+@adb reboot bootloader
+@fastboot boot C:\Lg-Manager\L5\recovery\recovery.img
+@adb wait-for-device
+@adb sideload %ROOT%
+echo Now phone will complete unlock.
+echo When it will become blank / says done
+pause
+@adb reboot
+echo Done! Succesfully unlocked!
+goto :startup
+
+
+:unlock32
+cls
+echo ################################
+echo # LG L Manager                 #
+echo # Selected device: %DEVICE%    #
+echo ################################
+echo.
+echo Full unlock - Lg l3 II e430
+echo.
+echo Unlocker by linuxxxx
+@adb reboot bootloader
+@fastboot devices
+@fastboot boot C:\Lg-Manager\L32\recovery\recovery.img
+@adb wait-for-device
+@adb push C:\Lg-Manager\L32/bootloader/lock.bin /sdcard
+@adb push C:\Lg-Manager\L32/bootloader/boot.img /sdcard
+@adb push C:\Lg-Manager\L32\recovery\recovery.img /sdcard
+@adb shell dd if=/sdcard/lock.bin of=/dev/block/platform/msm_sdcc.3/mmcblk0p5
+@adb shell dd if=/sdcard/boot.img of=/dev/block/platform/msm_sdcc.3/mmcblk0p9
+@adb shell dd if=/sdcard/recovery.img of=/dev/block/platform/msm_sdcc.3/mmcblk0p17
+@adb reboot bootloader
+@fastboot devices
+@fastboot flash recovery C:\Lg-Manager\L32\recovery\recovery.img
+@fastboot boot C:\Lg-Manager\L32\recovery\recovery.img
+@adb shell rm -rf /cache/recovery
+@adb shell mkdir /cache/recovery
+@adb shell "echo -e '--sideload' > /cache/recovery/command"
+@adb reboot bootloader
+@fastboot boot C:\Lg-Manager\L32\recovery\recovery.img
+@adb wait-for-device
+@adb sideload %ROOT%
+echo Now phone will complete unlock.
+echo When it will become blank / says done
+pause
+@adb reboot
+echo Done! Succesfully unlocked!
+goto :startup
+
+
+:about
+cls
+echo ################################
+echo # LG L Manager                 #
+echo # Selected device: %DEVICE%    #
+echo ################################
+echo.
+echo About
+echo.
+echo - License: Gpl V2
+echo - Developer: Joey Rizzoli
+echo - Device Supported: Lg L3, L5, L7, L3 II
+echo - Incoming Support: Lg L5 II, Lg L7 II
+echo - Disclaimer: this program may void your warranty. Developer disclaim every
+echo               damage caused from this program on your device.
+echo - Thanks: Aosp, some xda members (don't ask names), Lg, my devices
+echo - Based on: Xiaomi Toolkit [By Joey Rizzoli]
+echo - Sources:  https://github.com/ionolinuxnoparty/Lg-L-Manager
+echo.
+pause
+goto :startup
