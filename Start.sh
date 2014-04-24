1111f661c0900362c718d8e01adef8093bbab920
Joey Rizzoli <io.nolinuxnoparty@gmail.com>
2014-04-24 16:30:40 +0200

    Update Start.sh

--- Start.sh
+++ Start.sh
@@ -13,3 +13,105 @@
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
+
+L=~/Lg-LManager
+L3=~/Lg-LManager/e400
+L5=~/Lg-LManager/e610
+L7=~/Lg-LManager/p700
+L32=~/Lg-LManager/e430
+ROOTURL=http://download.chainfire.eu/372/SuperSU/UPDATE-SuperSU-v1.86.zip?retrieve_file=1
+ROOTZIP=~/res/root.zip
+DEVICE=
+ACTION=$1
+
+#----------------------------------------------------------------------------------------
+
+
+detect_device() {
+    echo "Connect device..."
+    DEVICE=$(adb wait-for-device shell getprop ro.product.device)
+
+    if [[ $? != 0 ]] ; then
+        echo "adb return error."
+        exit -1
+    fi
+
+    if [[ "$DEVICE" == e400* ]]; then
+      echo "Detected connected Lg L3"
+      DEVICE=E400
+    elif [[ "$DEVICE" == e610* ]]; then
+      echo "Detected connected Lg L5"
+      DEVICE=E610
+    elif [[ "$DEVICE" == p700* ]]; then
+      echo "Detected connected Lg L7"
+      DEVICE=p700
+    elif [[ "$DEVICE" == e430* ]]; then
+      echo "Detected connected Lg L3 II"
+      DEVICE=E430
+      else
+      echo "Connected device is not supported"
+      exit 0
+    fi
+}
+
+headerprint () {
+  clear
+  echo "################################"
+  echo "# LG L Manager                 #"
+  echo "# Selected device: $DEVICE        #"
+  echo "################################"
+  fi
+}
+
+home () {
+  echo "1- Install         4- Full Unlock"
+  echo "2- Backup          5- Shell"
+  echo "3- Push and Pull"
+  echo " "
+  echo "0- Exit            00-About"
+  echo " "
+  read -p "?" Choice
+  if  [ "$Choice" = "1" ]
+    then
+      install
+      break
+  elif  [ "$Choice" = "2" ]
+    then
+      back1
+      break
+  elif  [ "$Choice" = "3" ]
+    then
+      pnp
+      break
+  elif  [ "$Choice" = "4" ]
+    then
+      unlock
+      break
+  elif  [ "$Choice" = "5" ]
+    then
+      shelll
+      break
+  elif  [ "$Choice" = "0" ]
+    then
+      close
+      break
+  elif  [ "$Choice" = "00" ]
+    then
+      about
+      break
+  elif  [ "$Choice" = "make me a sandwich" ]
+    then
+      read -p "Do it yourself:" Choice
+      if [ "$Choice" = "sudo make me a sandwich" ]
+        then
+          read -p "OK"
+          break
+      else
+      echo "Wrong input"
+      fi
+      break
+  else
+  echo "Wrong input"
+  fi
+  home
+  }
